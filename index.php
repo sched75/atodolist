c096300f0eb83bfe41068b04d3ec71dd5c466361
PLD <sched@free.fr>
2016-06-19 08:57:41 +0000

    Add log management and create logs directory.

--- index.php
+++ index.php
@@ -4,11 +4,29 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+
+        'logger' => [
+            'name' => 'slim-app',
+            'level' => Monolog\Logger::DEBUG,
+            'path' => __DIR__ . '/../logs/app.log',
+        ],
+    ],
+];
+$app = new \Slim\App($config);
+$container=$app->getContainer();
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler(__DIR__."/logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-
+    $this->logger->addInfo("Something interesting happened");
     return $response;
 });
 $app->run();
